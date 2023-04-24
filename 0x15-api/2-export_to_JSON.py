5674ae440bb7d935c601eb0fb4c7f306d0bfa3de
KamauDev-maker <oscarnjenga@gmail.com>
2023-04-24 18:11:37 +0300

    correction for the output

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,19 +12,21 @@ if __name__ == '__main__':
     employee_id = int(sys.argv[1])
 
     user_info_url = "{}/users/{}".format(base_url, employee_id)
-    todo_list_url = user_info_url + "/todos"
-    user_info_response = requests.get(user_info_url).json()
-    todo_list_response = requests.get(todo_list_url).json()
-    username = user_info_response.get("username")
-    total_task = todo_list_response
-    completed_task_titles = []
-    for task in total_task:
-        id_completed = {}
-        id_completed["task"] = str(task.get("title"))
-        id_completed["completed"] = str(task.get("completed"))
-        id_completed["username"] = str(username)
-        completed_task_titles.append(id_completed)
-    completed = {}
-    completed[employee_id] = completed_task_titles
+    response = requests.get(user_info_url)
+    employee_info = response.json()
+    employee_name = employee_info["username"]
+
+    todo_list_url = "{}/todos?userId={}".format(base_url, employee_id)
+    response = requests.get(todo_list_url)
+    todo_list = response.json()
+
+    json_data = {}
+    json_data[employee_id] = []
+    for task in todo_list:
+        json_data[employee_id].append({
+            "task": task["title"],
+            "completed": task["completed"],
+            "username": employee_name
+            })
     with open("{}.json".format(employee_id), "w") as filejson:
-        filejson.write(json.dumps(completed))
+        json.dump(json_data, filejson)
