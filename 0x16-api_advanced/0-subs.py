dc27fcde7fb4ef24fa818e34fd2f71a2284bedcf
KamauDev-maker <oscarnjenga@gmail.com>
2023-05-09 13:53:10 +0300

    using the set guide of PEP 8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,10 @@ import the modules
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ set the custom User_agent"""
-    headers = { 'User-Agent': 'Google Chrome Version 112.0.5615.49 '}
+    headers = {'User-Agent': 'Google Chrome Version 112.0.5615.49'}
 
     url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers)
