4f0be8534368535fd363597c94a4c0219c72b00f
KamauDev-maker <oscarnjenga@gmail.com>
2023-06-23 20:21:57 +0300

    reviews

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-exec { 'fix_phpp':
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+  path    => '/usr/local/bin/:/bin/'
 }
